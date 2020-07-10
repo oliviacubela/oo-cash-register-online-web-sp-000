e738a17ef30401e5ac5eb309b49a185eef012bd6
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 16:29:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     @@shopping_cart << @items
   end
 
-  def add_item(title, price, total)
+  def add_item(title, price)
     self.total += price
     @@shopping_cart << price
   end
