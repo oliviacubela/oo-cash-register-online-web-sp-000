7868b3aa77e8d28d336f6d61994978c017e6b961
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 19:08:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,8 +25,15 @@ class CashRegister
 
 
   def apply_discount
-
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
   end
+
 
   def void_last_transaction
   end
