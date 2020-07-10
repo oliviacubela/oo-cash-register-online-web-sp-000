9e0fe2bd8e81d113326d3e227edb8cb9445c5748
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 19:40:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     self.total += price*quantity
-    @items << title += 1
+    @items << title
     # if quantity>1
     #   i=0
     #   while i<quantity
